c04173f09029119e3f326eb97606a5ecdc218e3a
Luke Douglas <lukedouglas1210@gmail.com>
2018-10-03 16:58:29 -0400

    initial late parse endpoit and supporting function

---
+++ routes/parse.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const upload = multer()
+const fs = require('fs')
+const es = require('event-stream')
+const { replaceStringInString, parseInputTextToStrings, createFile, deleteFile } = require('../supporting_functions.js')
+
+/* GET home page. */
+router.post('/', upload.single('text'), async function (req, res, next) {
+  let file = req.file
+  let input = parseInputTextToStrings(req.body.words)
+  await createFile(`./${file.originalname}`, file.buffer)
+  let read = fs.createReadStream(`./${file.originalname}`)
+  // let write = fs.createWriteStream('./testRedact')
+
+  read.pipe(es.split('.'))
+    .pipe(es.map(function (data, cb) {
+      let phrases = input.phrases
+      let words = input.words
+      let improvedData = data
+      if (data.length > 1) {
+        improvedData = data + '.'
+      }
+      phrases.forEach(phrase => {
+        improvedData = replaceStringInString(improvedData, phrase, 'xxxx')
+      })
+      words.forEach(word => {
+        improvedData = replaceStringInString(improvedData, word, 'xxxx')
+      })
+      cb(null, improvedData)
+    }))
+    .pipe(res)
+
+  res.on('finish', async function () {
+    await deleteFile(`./${file.originalname}`)
+  })
+})
+
+module.exports = router
---
+++ supporting_functions.js
@@ -0,0 +1,65 @@
+const _ = require('underscore')
+const fs = require('fs')
+const util = require('util')
+const writeFile = util.promisify(fs.writeFile)
+const unlinkFile = util.promisify(fs.unlink)
+// inputString - string - `String of keywords and phrases: a string of censored keywords and phrases separated by spaces or commas. Phrases will be enclosed in single or double quotes. Some examples: Hello world “Boston Red Sox”‘Pepperoni Pizza’, ‘Cheese Pizza’, beer`
+// returns - returns {words: [], phrases: [] }
+
+const parseInputTextToStrings = (inputString) => {
+  let singleQuoteMatches = inputString.match(/\'(.*?)\'/g) || []
+  let cleanedSingleQuoteMatches = _.map(singleQuoteMatches, string => {
+    return string.replace(/'/g, '')
+  })
+  // remove single sring matches
+  let doubleQuoteMatches = inputString.match(/\"(.*?)\"/g) || []
+  let cleanedDoubleQuoteMatches = _.map(doubleQuoteMatches, string => {
+    return string.replace(/"/g, '')
+  })
+  // remove double string matches
+  let inputStringWithoutPhrases = inputString
+  singleQuoteMatches.forEach(phrase => {
+    inputStringWithoutPhrases = inputStringWithoutPhrases.replace(phrase, '')
+  })
+  doubleQuoteMatches.forEach(phrase => {
+    inputStringWithoutPhrases = inputStringWithoutPhrases.replace(phrase, '')
+  })
+  // replace spaces with ,
+  let words = inputStringWithoutPhrases.replace(/,/g, ' ').split(' ')
+  words = _.filter(words, word => {
+    return word !== ''
+  })
+  let phrases = cleanedSingleQuoteMatches.concat(cleanedDoubleQuoteMatches)
+  return {
+    words: words,
+    phrases: phrases
+  }
+}
+// for a string replaces all instances of a search value with an new value
+const replaceStringInString = (textString, searchString, replaceValue) => {
+  let re = new RegExp('\\b' + searchString + '\\b', 'g')
+  let result = textString.replace(re, replaceValue)
+  return result
+}
+
+const createFile = async (path, fileString) => {
+  try {
+    await writeFile(path, fileString)
+  } catch (e) {
+    throw new Error(`writeFile failed with the following error: ${e.message}`)
+  }
+}
+
+const deleteFile = async (path) => {
+  try {
+    await unlinkFile(path)
+  } catch (e) {
+    throw new Error(`deleteFile failed with the following error: ${e.message}`)
+  }
+}
+module.exports = {
+  parseInputTextToStrings: parseInputTextToStrings,
+  replaceStringInString: replaceStringInString,
+  createFile: createFile,
+  deleteFile: deleteFile
+}
